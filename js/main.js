83c32bbd4bbc55f8cef32b322893818f8c75aafb
Agustin Castro <agus.castro915@gmail.com>
2021-09-13 16:29:33 -0300

    Funciones y entradas

--- js/main.js
+++ js/main.js
@@ -17,32 +17,49 @@ class Venta {
 ventas.push(new Venta(4000, 'Campera', 'Efectivo'));
 ventas.push(new Venta(2900, 'Buzo', 'Debito'));
 
-let resumen = 'VENTAS:\n';
-let totalGanancias = 0;
-
-for(let i = 0; i < ventas.length; i++){
-    const precioVenta = ventas[i].precio;
-    const categoriaVenta = ventas[i].categoria;
-    const pagoVenta = ventas[i].pago;
-    totalGanancias += precioVenta;
-    resumen += `Venta ${i + 1}. $${precioVenta} - ${categoriaVenta} - ${pagoVenta}\n`
+const ingresarVentas = () => {
+    let pregunta = prompt('Desea ingresar una venta?\n1- SI\n2- NO');
+    while(pregunta == 1){
+        const entradaPrecio = Number(prompt('Precio de la venta:'));
+        const entradaCategoria = prompt('Categoria de la venta:');
+        const entradaPago = prompt('Forma de pago:');
+        ventas.push(new Venta(entradaPrecio, entradaCategoria, entradaPago));
+        pregunta = prompt('Desea ingresar una venta?\n1- SI\n2- NO');
+    }
 }
 
-resumen += `----------------------\nTOTAL: $${totalGanancias}`
+ingresarVentas();
 
-// Copia del Array de ventas
-let arrOrdenado = ventas.slice();
+const resumenVentas = () => {
+    let resumen = 'VENTAS:\n';
+    let totalGanancias = 0;
+    
+    for(let i = 0; i < ventas.length; i++){
+        const precioVenta = ventas[i].precio;
+        const categoriaVenta = ventas[i].categoria;
+        const pagoVenta = ventas[i].pago;
+        totalGanancias += precioVenta;
+        resumen += `Venta ${i + 1}. $${precioVenta} - ${categoriaVenta} - ${pagoVenta}\n`
+    }
+    resumen += `----------------------\nTOTAL: $${totalGanancias}`;
 
-// Ordenar de Mayor a Menor
-arrOrdenado.sort((a, b) =>  b.precio - a.precio);
-
-let resumenOrdenado = 'MAYOR A MENOR:\n'
-
-console.log(arrOrdenado);
-for(let i = 0; i < arrOrdenado.length; i++){
-    resumenOrdenado += `${i + 1}. $${arrOrdenado[i].precio} - ${arrOrdenado[i].categoria} - ${arrOrdenado[i].pago}\n`
+    return resumen;
 }
 
+const resumenVentasOrd = () => {
+    // Copia del Array de ventas
+    let arrOrdenado = ventas.slice();
 
-console.log(resumen);
-console.log(resumenOrdenado);
+    // Ordenar de Mayor a Menor
+    arrOrdenado.sort((a, b) =>  b.precio - a.precio);
+
+    let resumenOrdenado = 'MAYOR A MENOR:\n'
+
+    for(let i = 0; i < arrOrdenado.length; i++){
+        resumenOrdenado += `${i + 1}. $${arrOrdenado[i].precio} - ${arrOrdenado[i].categoria} - ${arrOrdenado[i].pago}\n`
+    }
+    return resumenOrdenado;
+}
+
+console.log(resumenVentas());
+console.log(resumenVentasOrd());
