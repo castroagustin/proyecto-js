4977ae8be5ff76b0c4d5d84ffd3ed46a08a3d6b6
Agustin Castro <agus.castro915@gmail.com>
2021-09-27 13:29:46 -0300

    2. DOM, JSON, Storage

--- js/main.js
+++ js/main.js
@@ -1,65 +1,113 @@
-const ventas = [
-    {precio: 8000, categoria: 'Zapatillas', pago: 'Credito'},
-    {precio: 4500, categoria: 'Pantalon', pago: 'Debito'},
-    {precio: 1900, categoria: 'Remera', pago: 'Debito'}
-]
+const productsList = document.querySelector('.products__list');
+const agregarBtn = document.querySelector('.products__agregarBtn');
+const borrarBtn = document.querySelector('.products__borrarBtn');
+const modalInput = document.querySelector('.modal--input');
+const modalMessage = document.querySelector('.modal--message');
+const overlay = document.querySelector('.overlay');
+const modalSubmitInput = document.querySelector('.modal__submitInput');
+const modalSubmitMessage = document.querySelector('.modal__submitMessage');
+const modalCancelMessage = document.querySelector('.modal__cancelMessage');
+const modalClear = document.querySelector('.modal__clear');
+const closeModalBtn = document.querySelector('.modal__close');
 
-// Constructor de ventas
-class Venta {
-    constructor(precio, categoria, pago){
-        this.precio = precio;
+const inputProd = document.querySelector('.producto__nombre');
+const inputCateg = document.querySelector('.producto__categoria');
+const inputPrecio = document.querySelector('.producto__precio');
+const inputStock = document.querySelector('.producto__stock');
+
+let productos = JSON.parse(localStorage.getItem('products')) || [];
+
+// Constructor de productos
+class Producto {
+    constructor(nombre, categoria, precio, stock, id){
+        this.nombre = nombre;
         this.categoria = categoria;
-        this.pago = pago;
+        this.precio = precio;
+        this.stock = stock;
+        this.id = id;
+        this.ventas = 0;
     }
+    hayStock = (cant) => this.stock >= cant;
+    restarStock = (cant) => this.stock -= cant;
+    sumarStock = (cant) => this.stock += cant;
+    sumarVentas = (cant) => this.ventas += cant;
 }
 
-// Agregar ventas
-ventas.push(new Venta(4000, 'Campera', 'Efectivo'));
-ventas.push(new Venta(2900, 'Buzo', 'Debito'));
+agregarBtn.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+    modalInput.classList.remove('hidden');
+});
 
-const ingresarVentas = () => {
-    let pregunta = prompt('Desea ingresar una venta?\n1- SI\n2- NO');
-    while(pregunta == 1){
-        const entradaPrecio = Number(prompt('Precio de la venta:'));
-        const entradaCategoria = prompt('Categoria de la venta:');
-        const entradaPago = prompt('Forma de pago:');
-        ventas.push(new Venta(entradaPrecio, entradaCategoria, entradaPago));
-        pregunta = prompt('Desea ingresar una venta?\n1- SI\n2- NO');
-    }
+borrarBtn.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+    modalMessage.classList.remove('hidden');
+});
+
+overlay.onclick = () => {
+    overlay.classList.add('hidden');
+    modalInput.classList.add('hidden');
+    modalMessage.classList.add('hidden');
+    clearValues();
+};
+
+closeModalBtn.onclick = () => {
+    overlay.classList.add('hidden');
+    modalInput.classList.add('hidden');
+    modalMessage.classList.add('hidden');
+    clearValues();
+};
+
+modalSubmitInput.onclick = e => {
+    e.preventDefault();
+    agregarProducto();
+};
+
+modalClear.onclick = () => clearValues();
+
+modalCancelMessage.onclick = () => {
+    overlay.classList.add('hidden');
+    modalMessage.classList.add('hidden');
 }
 
-ingresarVentas();
-
-const resumenVentas = () => {
-    let resumen = 'VENTAS:\n';
-    let totalGanancias = 0;
-    
-    for(let i = 0; i < ventas.length; i++){
-        const precioVenta = ventas[i].precio;
-        const categoriaVenta = ventas[i].categoria;
-        const pagoVenta = ventas[i].pago;
-        totalGanancias += precioVenta;
-        resumen += `Venta ${i + 1}. $${precioVenta} - ${categoriaVenta} - ${pagoVenta}\n`
-    }
-    resumen += `----------------------\nTOTAL: $${totalGanancias}`;
-
-    return resumen;
+modalSubmitMessage.onclick = () => {
+    productos = [];
+    localStorage.setItem('products', JSON.stringify(productos));
+    mostrarProductos();
+    overlay.classList.add('hidden');
+    modalMessage.classList.add('hidden');
 }
 
-const resumenVentasOrd = () => {
-    // Copia del Array de ventas
-    let arrOrdenado = ventas.slice();
-
-    // Ordenar de Mayor a Menor
-    arrOrdenado.sort((a, b) =>  b.precio - a.precio);
-
-    let resumenOrdenado = 'MAYOR A MENOR:\n'
-
-    for(let i = 0; i < arrOrdenado.length; i++){
-        resumenOrdenado += `${i + 1}. $${arrOrdenado[i].precio} - ${arrOrdenado[i].categoria} - ${arrOrdenado[i].pago}\n`
-    }
-    return resumenOrdenado;
+const clearValues = () => {
+    inputProd.value = '';
+    inputCateg.value = '';
+    inputPrecio.value = '';
+    inputStock.value = '';
 }
 
-console.log(resumenVentas());
-console.log(resumenVentasOrd());
+const agregarProducto = () => {
+    const prodNombre = inputProd.value;
+    const prodCateg = inputCateg.value;
+    const prodPrecio = inputPrecio.value;
+    const prodStock = inputStock.value;
+    const prodId = productos.length + 1;
+    const objProd = new Producto(prodNombre, prodCateg, prodPrecio, prodStock, prodId);
+    productos.push(objProd);
+    localStorage.setItem('products', JSON.stringify(productos));
+    mostrarProductos();
+}
+
+const mostrarProductos = () => {
+    const prodContainer = document.createElement('div');
+    productos.forEach((prod) => {
+        prodContainer.innerHTML += `
+        <div class='list__row' id='${prod.id}'>
+            <span class="row__text row__text--left">${prod.nombre}</span>
+            <span class="row__text row__text--center">$${prod.precio}</span>
+            <span class="row__text row__text--right">${prod.stock}</span>
+        </div>
+        `;
+    })
+    productsList.innerHTML = prodContainer.innerHTML;
+}
+
+mostrarProductos();
